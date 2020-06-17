efd6ba5e9a4d5db63be11738f8a7742f1758c52f
dengs.official <dengs.official@gmail.com>
2020-06-17 22:43:34 +0800

    添加主题修改

--- src/index.js
+++ src/index.js
@@ -5,9 +5,9 @@ import * as serviceWorker from './serviceWorker';
 import './styles/index';
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  // <React.StrictMode>
+  <App />,
+  // </React.StrictMode>,
   document.getElementById('root'),
 );
 
--- src/pages/layout/components/header/RHeader.jsx
+++ src/pages/layout/components/header/RHeader.jsx
@@ -9,8 +9,11 @@ const { Header } = Layout;
 function RHeader() {
   return (
     <Header>
-      <Menu>
-        men
+
+      <Menu theme="dark" mode="horizontal">
+        <Menu.Item key="1">nav 1</Menu.Item>
+        <Menu.Item key="2">nav 2</Menu.Item>
+        <Menu.Item key="3">nav 3</Menu.Item>
       </Menu>
     </Header>
   );
